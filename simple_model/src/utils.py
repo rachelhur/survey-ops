697ccd100251e0b7cb9ed63a0e2fd128232c6036
hurrachel <hurrachel@gmail.com>
2025-11-05 22:24:46 -0600

    update to pull from laptop

--- simple_model/src/utils.py
+++ simple_model/src/utils.py
@@ -1,13 +1,3 @@
 import pickle
-import os
+import os  
 
-
-def save_results(results: dict, fp: str):
-    if not os.path.exists:
-        os.makedirs('results')
-    with open(fp, 'wb') as handle:
-        pickle.dump(results, fp)
-
-def load_results(fp: str):
-    with open(fp, 'rb') as handle:
-        return pickle.load(fp)
