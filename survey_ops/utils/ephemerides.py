a6c37c9704fd3f88f88117af5900d3fcbbb8a732
Paul Chichura <pchichura@uchicago.edu>
2025-11-07 14:30:43 -0600

    convert between equatorial and topographic coords

--- survey_ops/utils/ephemerides.py
+++ survey_ops/utils/ephemerides.py
@@ -1,6 +1,36 @@
 import ephem
 from datetime import datetime
 
+def blanco_observer(time=None):
+    """
+    Define an ephem.Observer for the Blanco telescope location. Location info from:
+    https://www.ctio.noirlab.edu/noao/content/coordinates-observatories-cerro-tololo-and-cerro-pachon
+
+    Arguments
+    ---------
+    time : float [None]
+        Time (Unix timestamp, in UTC) at which to define observer. Default: now.
+
+    Returns
+    -------
+    observer : ephem.Observer
+        Ephem observer object for Blanco location at specified time.
+    """
+
+    # define the location
+    observer = ephem.Observer()
+    observer.lat = "-30:10:10.78" # -30.169661 deg
+    observer.lon = "-70:48:23.49" # -70.806525 deg
+    observer.elevation = 2206.8  # m
+
+    # initialize time
+    if time is None:
+        observer.date = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
+    else:
+        observer.date = datetime.fromtimestamp(time).strftime("%Y/%m/%d %H:%M:%S")
+
+    return observer
+
 def get_source_ra_dec(source, time=None):
     """
     Get the astrophysical coordinates of a known source using pyephem.
@@ -18,19 +48,10 @@ def get_source_ra_dec(source, time=None):
         Source position in radians
     """
 
-    # define BLANCO observer location and time
-    # http://www.ctio.noao.edu/noao/content/coordinates-observatories-cerro-tololo-and-cerro-pachon
-    observer = ephem.Observer()
-    observer.lat = '-30:10:10.78'
-    observer.lon = '-70:48:23.49'
-    observer.elevation = 2206.8 # m
-    if time is None:
-        observer.date = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
-    else:
-        observer.date = datetime.fromtimestamp(time).strftime("%Y/%m/%d %H:%M:%S")
+    # create observer at Blanco telescope
+    observer = blanco_observer(time=time)
 
     # grab ephem object for the source
-    body = None
     source = source.lower()
     if source == "moon":
         body = ephem.Moon()
@@ -42,3 +63,59 @@ def get_source_ra_dec(source, time=None):
     # compute source location
     body.compute(observer)
     return body.ra, body.dec
+
+def equatorial_to_topographic(ra, dec, time=None):
+    """
+    Convert RA/Dec to Az/El for the Blanco telescope location.
+
+    Arguments
+    ---------
+    ra : float
+        Right ascension in radians
+    dec : float
+        Declination in radians
+    time : float [None]
+        Time (Unix timestamp, in UTC) at which to determine position. Default: now.
+
+    Returns
+    -------
+    az, el : float
+        Azimuth and elevation in radians
+    """
+
+    # initialize observer location and time
+    observer = blanco_observer(time=time)
+
+    # define position in equatorial coordinates
+    source = ephem.FixedBody()
+    source._ra = ra
+    source._dec = dec
+
+    # compute topographic position for the observer
+    source.compute(observer)
+    return source.az, source.alt
+
+def topographic_to_equatorial(az, el, time=None):
+    """
+    Convert Az/El to RA/Dec for the Blanco telescope location.
+
+    Arguments
+    ---------
+    az : float
+        Azimuth in radians
+    el : float
+        Elevation in radians
+    time : float [None]
+        Time (Unix timestamp, in UTC) at which to determine position. Default: now.
+
+    Returns
+    -------
+    ra, dec : float
+        Right ascension and declination in radians
+    """
+
+    # initialize observer location and time
+    observer = blanco_observer(time=time)
+
+    # compute topographic position for the observer
+    return observer.radec_of(az, el)
