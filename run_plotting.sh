addaeb5a6dff3283e47f22be81d1b351ff47af95
Rachel Hur <hurrachel@gmail.com>
2026-01-06 16:55:52 -0600

    rewrite for updates to plotting.py

--- run_plotting.sh
+++ run_plotting.sh
@@ -1,1 +1,12 @@
-python /home/rachel/Projects/survey-ops/survey_ops/plotting.py -o my_test_schedule.gif -f /home/rachel/Projects/survey-ops/data/fields2radec.json -s /home/rachel/Projects/survey-ops/data/true_schedule.csv
+# original command -- test
+# python /home/rachel/Projects/survey-ops/survey_ops/plotting.py -o my_test_schedule.gif -f /home/rachel/Projects/survey-ops/data/fields2radec.json -s /home/rachel/Projects/survey-ops/data/true_schedule.csv
+
+
+# python /home/rachel/Projects/survey-ops/survey_ops/plotting.py -o /home/rachel/Projects/survey-ops/results/v2f-environment-testing/original_field_schedule.gif -f /home/rachel/Projects/survey-ops/data/fields2radec.json -s /home/rachel/Projects/survey-ops/data/true_schedule.csv
+# Plot field schedule
+    # original schedule:
+# python /home/rachel/Projects/survey-ops/survey_ops/plotting.py -o /home/rachel/Projects/survey-ops/results/v2f-environment-testing/original_field_schedule.gif -f /home/rachel/Projects/survey-ops/data/field2radec.json -s /home/rachel/Projects/survey-ops/results/v2f-environment-testing/original_schedule.csv
+    # policy roll-out schedule:
+# python /home/rachel/Projects/survey-ops/survey_ops/plotting.py -o /home/rachel/Projects/survey-ops/results/v2f-environment-testing/eval_field_schedule.gif -f /home/rachel/Projects/survey-ops/data/field2radec.json -s /home/rachel/Projects/survey-ops/results/v2f-environment-testing/eval_schedule.csv
+# plot bins:
+python /home/rachel/Projects/survey-ops/survey_ops/plotting.py -f /home/rachel/Projects/survey-ops/data/nside16_bin2radec.json -o /home/rachel/Projects/survey-ops/results/v2f-environment-testing/bin_schedule.gif -b /home/rachel/Projects/survey-ops/results/v2f-environment-testing/bin_schedule.csv -n 16
