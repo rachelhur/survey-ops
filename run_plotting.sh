da65745269eb31d1239ab81cddcdc1442857af18
hurrachel <hurrachel@gmail.com>
2026-01-12 15:28:12 -0600

    re-write run_plotting.py for checks on new schedules created with script-based pipeline

--- run_plotting.sh
+++ run_plotting.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 current_dir=$(pwd)
-result_dir="v2f-environment-testing"
+result_dir="evaluation_1/2018-12-1/"
+nside=16
 
 # original command -- test
 # python /home/rachel/Projects/survey-ops/survey_ops/plotting.py -o my_test_schedule.gif -f /home/rachel/Projects/survey-ops/data/fields2radec.json -s /home/rachel/Projects/survey-ops/data/true_schedule.csv
@@ -14,4 +15,4 @@ result_dir="v2f-environment-testing"
 # python $current_dir/plotting.py -o $current_dir/results/v2f-environment-testing/eval_field_schedule.gif -f $current_dir/data/field2radec.json -s $current_dir/results/v2f-environment-testing/eval_schedule.csv
 # plot bins:
 echo "Creating bin schedule gif"
-python "$current_dir/survey_ops/plotting.py" -f "$current_dir/data/nside16_bin2radec.json" -o "$current_dir/results/$result_dir/bin_schedule.gif" -b "$current_dir/results/$result_dir/bin_schedule.csv" -n 16
+python "$current_dir/survey_ops/plotting_alt.py" -f "$current_dir/data/nside${nside}_bin2radec.json" -o "$current_dir/experiment_results/test_experiment/$result_dir/bin_schedule.gif" -b "$current_dir/experiment_results/test_experiment/$result_dir/bin_schedule.csv" -n $nside
