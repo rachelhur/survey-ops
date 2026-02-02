54b7d2c8f98049bb656a95c680af1371848367f0
Rachel Hur <hurrachel@gmail.com>
2026-02-01 23:10:49 -0600

    include config.py file (forgot with last commit)

---
+++ survey_ops/utils/config.py
@@ -0,0 +1,38 @@
+import json
+
+class Config:
+    def __init__(self, config_path):
+        self.config_path = config_path
+        with open(config_path) as f:
+            self._data = json.load(f)
+
+    def get(self, path=None, default=None):
+        if path is None:
+            return self._data
+
+        node = self._data
+        for key in path.split("."):
+            if not isinstance(node, dict):
+                return default
+            node = node.get(key, default)
+            if node is default:
+                return default
+        return node
+    
+    def set(self, path, value):
+        keys = path.split(".")
+        node = self._data
+
+        # walk to parent
+        for key in keys[:-1]:
+            if key not in node or not isinstance(node[key], dict):
+                node[key] = {}
+            node = node[key]
+
+        node[keys[-1]] = value
+
+    def save(self, path=None, indent=2):
+        path = path or self.config_path
+        with open(path, "w") as f:
+            json.dump(self._data, f, indent=indent)
+
